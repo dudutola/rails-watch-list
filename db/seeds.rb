54fab86b114c359549ecd37590f5c79be6c9c69e
Dulcelene Machado <dudutola@gmail.com>
2025-03-01 10:00:48 +0000

    Seed movies from The Movie Database API

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'open-uri'
+require 'json'
+
+api_url = "https://tmdb.lewagon.com/movie/top_rated"
+
+movies_data = JSON.parse(URI.open(api_url).read)
+
+movies_data["results"].each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "Movies seeded successfully!"
