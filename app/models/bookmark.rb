380446af54c04a1f4375fecc361b9605950feb43
Dulcelene Machado <dudutola@gmail.com>
2025-03-01 09:56:12 +0000

    Added validations to model bookmark

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,10 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :movie, presence: true
+  validates :list, presence: true
+  validates :comment, presence: true, length: { minimum: 6 }
+
+  validates_uniqueness_of :movie_id, scope: :list_id
 end
