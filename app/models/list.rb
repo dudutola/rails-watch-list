99bac7cb712e21c99dbb40a0f0f623651181eecf
Dulcelene Machado <dudutola@gmail.com>
2025-03-01 10:42:40 +0000

    Added migration to let users add an image when creating a new list Styled the movies cards in lists#show Reviewed banner error in lists#show

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,16 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :image
 
   validates :name, presence: true, uniqueness: true
+  validate :correct_image_mime_type
+
+  private
+
+  def correct_image_mime_type
+    if image.attached? && !image.content_type.in?(%w[image/jpeg image/png])
+      errors.add(:image, "must be a JPEG or PNG")
+    end
+  end
 end
